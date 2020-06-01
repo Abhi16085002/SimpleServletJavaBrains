5b110b1683da30fa3a3095eeba5852f0f46e1ba7
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-06-01 22:03:56 +0530

    JSPs and Servlets Tutorial 03 - Understanding the Servlet

--- src/com/blogspot/javabrains/SimpleServlet.java
+++ src/com/blogspot/javabrains/SimpleServlet.java
@@ -1,8 +1,6 @@
-package org.koushik.javabrains;
+package com.blogspot.javabrains;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet(description = "A simple Servlet", urlPatterns = { "/SimpleServletPath" })
+@WebServlet(description = "Simple servlet", urlPatterns = { "/SimpleServlet" })
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -20,12 +18,18 @@ public class SimpleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		 TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("Hello from GET method");
-		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		writer.println("<h3>Hello in html</h3>");
+		System.out.println("Hello from Servlet GET method");
+		response.getWriter().print("Hello from servlet GET method - on the WEB page !");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
