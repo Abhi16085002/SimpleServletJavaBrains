153ceed9d67043b70af0cd015cc28613b0e9bb41
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-06-04 20:56:52 +0530

    JSPs and Servlets Tutorial 08 - Request, Session and Context Part 1

--- src/com/blogspot/javabrains/XmlServlet.java
+++ src/com/blogspot/javabrains/XmlServlet.java
@@ -7,14 +7,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class XmlServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		PrintWriter writer = response.getWriter();
 		String userName = request.getParameter("userName");
-		out.println("Hello! from GET  method" + userName );
+		HttpSession session = request.getSession();
+		if( userName!="" && userName!=null ) {
+			session.setAttribute("savedUserName", userName);
+		}
+		writer.println("request parameter has userName as " + userName );
+		writer.println("Seeion parameter has userName as " +  (String)session.getAttribute("savedUserName") );
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,6 +34,7 @@ public class XmlServlet extends HttpServlet {
 //		String location = request.getParameter("location");
 		String[] location = request.getParameterValues("location");
 		out.println("You are a " + location.length );
+		
 		
 	}
 	
