83875d230f437539718b7e7bc1ec7a90a8606fdf
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-06-04 21:59:41 +0530

    JSPs and Servlets Tutorial 08 - Request, Session and Context Part 2

--- src/com/blogspot/javabrains/XmlServlet.java
+++ src/com/blogspot/javabrains/XmlServlet.java
@@ -3,6 +3,7 @@ package com.blogspot.javabrains;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,11 +17,14 @@ public class XmlServlet extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		String userName = request.getParameter("userName");
 		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
 		if( userName!="" && userName!=null ) {
 			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
 		}
 		writer.println("request parameter has userName as " + userName );
 		writer.println("Seeion parameter has userName as " +  (String)session.getAttribute("savedUserName") );
+		writer.println("context parameter has userName as " +  (String)context.getAttribute("savedUserName") );
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
