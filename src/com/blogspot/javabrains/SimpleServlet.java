8d3a6f0824a5c27159fc11b77a0c6c121e3602b8
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-06-05 00:19:32 +0530

    JSPs and Servlets Tutorial 09 Part 3- Understanding init, service and ServletConfig

--- src/com/blogspot/javabrains/SimpleServlet.java
+++ src/com/blogspot/javabrains/SimpleServlet.java
@@ -1,16 +1,23 @@
 package com.blogspot.javabrains;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet(description = "Simple servlet", urlPatterns = { "/SimpleServlet" })
+@WebServlet(description = "Simple servlet", urlPatterns = { "/SimpleServlet" },
+				initParams= {@WebInitParam(name = "defaultUser" , value = "John doe") }
+		)
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -18,10 +25,21 @@ public class SimpleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("Hello from Servlet GET method");
-		response.getWriter().print("Hello from servlet GET method - on the WEB page !");
+		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		String userName = request.getParameter("userName");
+		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
+		if( userName!="" && userName!=null ) {
+			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
+		}
+		writer.println("request parameter has userName as " + userName );
+		writer.println("Seeion parameter has userName as " +  (String)session.getAttribute("savedUserName") );
+		writer.println("context parameter has userName as " +  (String)context.getAttribute("savedUserName") );
+		writer.println( "Init parameter has userName as " +  getServletConfig().getInitParameter("defaultUser") );
+		
 	}
 
 	/**
